04d1a63956d485fc85f37ad53cd62315b11ab067
marcohua <SUITE@SUITEBE308-ST10.cibertec.edu.pe>
2023-09-17 09:35:59 -0500

    commit inicial

---
+++ src/test/java/pe/edu/cibertec/appwebformularios/AppWebFormulariosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformularios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFormulariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
