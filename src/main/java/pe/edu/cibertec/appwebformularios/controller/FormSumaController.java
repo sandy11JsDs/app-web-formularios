436eb23eede3738afe89eef2be2e0c2b0c3158aa
marcohua <SUITE@SUITEBE308-ST10.cibertec.edu.pe>
2023-09-17 12:04:32 -0500

    desarrollo del ejercicio promedio

--- src/main/java/pe/edu/cibertec/appwebformularios/controller/FormSumaController.java
+++ src/main/java/pe/edu/cibertec/appwebformularios/controller/FormSumaController.java
@@ -1,12 +1,39 @@
 package pe.edu.cibertec.appwebformularios.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.appwebformularios.model.PromedioModel;
 
 @Controller
 public class FormSumaController {
     @GetMapping("/promedionotas")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("verresultado",false);
+        model.addAttribute("promediomodel",
+                new PromedioModel());
+        return "formSuma";
+    }
+    @PostMapping("/promedionotas")
+    public String calcularPromedioNotas(
+            @ModelAttribute("promediomodel")
+            PromedioModel promedioModel,
+            Model model){
+        Double promedio = (promedioModel.getCl1()*0.15)
+                + (promedioModel.getCl2()*0.20)
+                + (promedioModel.getCl3()*0.35)
+                +(promedioModel.getProyfin()*0.30);
+        String estado = promedio > 10.49 ? "Aprobado"
+                : "Desaprobado";
+        model.addAttribute("resultado",
+                "Su promedio de notas es: "
+                +promedio+", usted se encuentra: "
+                +estado);
+        model.addAttribute("verresultado",
+                true);
+        model.addAttribute(new PromedioModel());
         return "formSuma";
     }
 }
---
+++ src/main/java/pe/edu/cibertec/appwebformularios/model/PromedioModel.java
@@ -0,0 +1,15 @@
+package pe.edu.cibertec.appwebformularios.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class PromedioModel {
+    private Integer cl1;
+    private Integer cl2;
+    private Integer cl3;
+    private Integer proyfin;
+
+}
