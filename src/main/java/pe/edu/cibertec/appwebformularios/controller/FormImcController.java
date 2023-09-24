91a4094e67776c7c7b937b9dbca8d1f764d487c6
Zagaceta123 <rz_sebas@hotmail.com>
2023-09-24 10:56:27 -0500

    ejercicio 2 terminado

--- src/main/java/pe/edu/cibertec/appwebformularios/controller/FormImcController.java
+++ src/main/java/pe/edu/cibertec/appwebformularios/controller/FormImcController.java
@@ -1,4 +1,50 @@
 package pe.edu.cibertec.appwebformularios.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.appwebformularios.model.ImcModel;
+
+@Controller
 public class FormImcController {
+    @GetMapping("/calcularimc")
+    public String index(Model model){
+        model.addAttribute("imcmodel", new ImcModel());
+        model.addAttribute("verresultado", false);
+        return "formImc";
+    }
+    @PostMapping("/calcularimc")
+    public String calcularimc(@ModelAttribute("imcmodel")
+                              ImcModel imcmodel,
+                              Model model){
+        Double tallam = imcmodel.getAltura()/100;
+        Double valorimc = imcmodel.getPeso() / (tallam * tallam);
+        String resultadoimc = "";
+        String coloralert = "alert-danger";
+        if(valorimc <= 18.5){
+            resultadoimc = "Por debajo del peso.";
+            coloralert = "alert-dark";
+        } else if (valorimc <= 25) {
+            resultadoimc = "Con peso normal.";
+            coloralert = "alert-primary";
+        } else if (valorimc <= 30) {
+            resultadoimc = "Con sobrepeso";
+            coloralert = "alert-warning";
+        } else if (valorimc <= 35) {
+            resultadoimc = "Con obesidad leve";
+        } else if (valorimc <= 39) {
+            resultadoimc = "Con obesidad media";
+        }else {
+            resultadoimc = "Con obesidad mórbida";
+        }
+        model.addAttribute("verresultado", true);
+        model.addAttribute("resultado",
+                "Su valor de IMC es:" +
+                        String.format("%.2f", valorimc)
+                + ", usted se encuentra: " + resultadoimc);
+        model.addAttribute("coloralert", coloralert);
+        return "formImc";
+    }
 }
---
+++ src/main/java/pe/edu/cibertec/appwebformularios/model/ImcModel.java
@@ -0,0 +1,15 @@
+package pe.edu.cibertec.appwebformularios.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+
+public class ImcModel {
+
+    private Double peso;
+    private Double altura;
+
+}
